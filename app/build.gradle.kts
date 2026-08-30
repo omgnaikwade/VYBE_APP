369a047de757d1ce99ede715c34a00bbf5f0e9d2
omgnaikwade <omg.naikwade@gmail.com>
2026-08-30 19:11:50 +0530

    Add androidx.media dependency to build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -101,6 +101,7 @@ dependencies {
   implementation(libs.androidx.compose.ui.graphics)
   implementation(libs.androidx.compose.ui.tooling.preview)
   implementation("androidx.core:core-ktx:1.18.0")
+  implementation("androidx.media:media:1.7.0")
 
   // Lifecycle / Navigation
   implementation(libs.androidx.lifecycle.runtime.compose)
